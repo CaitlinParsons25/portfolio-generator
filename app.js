55347c5af225d6c3cd5f2c7ea9de95c1273b4e5a
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-06-30 23:19:55 -0500

    added site generation feature

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import inquirer from 'inquirer';
-import fs from 'fs';
+import { writeFile, copyFile} from './utils/generate-site.js';
 import generatePage from './src/page-template.js';
 
 // const pageHTML =  generatePage(name, github);
@@ -140,11 +140,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-         const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-    //   console.log('Page created! Check out index.html in this directory to see it!');
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-});
